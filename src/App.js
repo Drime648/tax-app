584fdb3d6a75574bce53649ca82555e528474c54
drime <dhruv.kandula@gmail.com>
2021-03-13 17:49:37 -0800

    add starting files

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      searchField: 0
+    }
+
+  }
+
+  onSearchChange = (event) => {
+    this.setState({searchField: event.target.value})
+  }
+
+  onButtonClicked = () => {
+    
+  }
+
+
+
+  render() {
+    
+
+    return (
+      
+
+
+      <div>
+        <form className="measure center">
+
+
+          <div className="mt3">
+            <label className="db fw6 lh-copy f6 tc f4 fw6 ph0 mh0" htmlFor="user-name">Yearly Income</label>
+              <input 
+              type = "number"
+              onChange = {this.onSearchChange}
+              className="pa2 input-reset ba bg-transparent w-100" 
+              name="income"  
+              id="income"/>
+          </div>
+          <br></br>
+
+
+
+
+          <div className = "center">
+            <input 
+              className="center b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+              type="submit" 
+              value="submit"
+              onClick = {() => this.onButtonClicked()}/>
+          </div>
+
+      
+        
+        </form>
+      </div>
+    )
+  }
+
+
+}
+
+
+
+export default App;
